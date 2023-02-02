912a68476bdff034b2cfa608cbe44965d5dedbf6
David <110458896+henriikkanorra@users.noreply.github.com>
2023-02-02 13:38:30 +0100

    clean js code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline({ defaults: { ease:'power1.out' }});
 
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 3, delay: 0.5 });
-tl.fromTo(".title", { opacity: 0 }, { opacity: 1, duration: 3 }, "-=3");
-tl.fromTo(".hero-buttons", { opacity: 0 }, { opacity: 1, duration: 3 }, "-=3");
+//hero page
+
+tl.fromTo(["nav", ".title", ".hero-buttons"], { opacity: 0 }, { opacity: 1, duration: 3, delay: 0.5});
